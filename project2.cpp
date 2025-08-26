deafa9293a6d271d4b8a9a7e2a56ef21941b3941
Muhammad Taha <wwwtahataha343@gmail.com>
2025-08-26 22:06:17 +0500

    Update project2.cpp

--- project2.cpp
+++ project2.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
 using namespace std;
 
-class user {
+class User {
 public:
     string user_name;
     string user_email;
 
-    void user_details() {
+    void getUserDetails() {
         cout << "Enter your name: ";
         cin >> user_name;
         cout << "Enter your working email: ";
@@ -15,184 +15,108 @@ public:
     }
 };
 
-class movie : public user {
+class Movie : public User {
 public:
-    int movieChoice, movieLanguageChoice, backSeats,ticket_price;
+    int moviechoice, movielanguagechoice, backSeats, ticket_price;
     string movie_name, movieTime;
     int dayChoice;
-    string dayName;
-    string movieLanguage;
+    string dayname;
+    string movielanguage;
+    int availableSeats;
 
-    void movie_days() {
-        cout << "Movies available for 3 days \n\n1.Friday\n\n2.Saturday\n\n3.Sunday\n\nFor which day you want to book your tickets: ";
+    void choose_movie() {
+        cout << "Movies available for 3 days \n\n1. Friday\n2. Saturday\n3. Sunday\n\nChoose day: ";
         cin >> dayChoice;
 
         switch (dayChoice) {
-            case 1:
-			 dayName = "Friday";
-			  break;
-            case 2:
-			 dayName = "Saturday";
-			  break;
-            case 3:
-			 dayName = "Sunday";
-			  break;
-            default: {
-			 cout << "wrong choice ";
-			  break;
-			  
-			   }
+            case 1: dayname = "Friday"; break;
+            case 2: dayname = "Saturday"; break;
+            case 3: dayname = "Sunday"; break;
+            default: cout << "Invalid day selected:";
         }
 
-        cout << "\nWhat type of movie would you like to watch\n\n1.English \n\n2.Punjabi \n\n";
-        cin >> movieLanguageChoice;
+        cout << "\nSelect movie language:\n1. English\n2. Punjabi\nChoose: ";
+        cin >> movielanguagechoice;
 
-        switch (movieLanguageChoice) {
-            case 1:
-			 movieLanguage = "English";
-			  break;
-            case 2:
-			 movieLanguage = "Punjabi";
-			  break;
-            default: { 
-			cout << "wrong choice ";
-			 break;
-			  }
+        switch (movielanguagechoice) {
+            case 1: movielanguage = "English"; break;
+            case 2: movielanguage = "Punjabi"; break;
+            default: cout << "Invalid choice";
         }
 
-        if (movieLanguageChoice == 1) {
-            cout << "Available movies for 3 days (Friday, Saturday, Sunday):\n";
-            cout << "1. Interstellar\n2. AVATAR\n3. Cyber Hell\nChoose (1-3): ";
-            cin >> movieChoice;
-
-            int time;
-            cout << "\nSelect the movie show time (1-3):\n";
-            cout << "1. 08:00 AM\n2. 03:00 PM\n3. 07:00 PM\n";
-            cin >> time;
-
-            switch (time) {
-                case 1:
-				 movieTime = "08:00 AM";
-				  break;
-                case 2:
-				 movieTime = "03:00 PM";
-				  break;
-                case 3:
-				 movieTime = "07:00 PM";
-				  break;
-                default: movieTime = "Unknown"; cout << "Invalid movie choice.\n";
+        if (movielanguage == "English") {
+            cout << "\nAvailable English movies:\n1. Interstellar\n2. Avatar\n3. Cyber Hell\nChoose (1-3): ";
+            cin >> moviechoice;
+            switch (moviechoice) {
+                case 1: movie_name = "Interstellar"; break;
+                case 2: movie_name = "Avatar"; break;
+                case 3: movie_name = "Cyber Hell"; break;
+                default: movie_name = "Unknown"; cout << "Invalid choice.\n";
             }
-
-            switch (movieChoice) {
-                case 1:
-				 movie_name = "Interstellar";
-				  break;
-                case 2:
-				 movie_name = "AVATAR";
-				  break;
-                case 3:
-				 movie_name = "Cyber Hell";
-				  break;
-                default: movieTime = "Unknown"; cout << "Invalid movie choice.\n";
+        } else {
+            cout << "\nAvailable Punjabi movies:\n1. Sardaar Ji 3\n2. Chal Mera Putt\n3. Jatt & Juliet\nChoose (1-3): ";
+            cin >> moviechoice;
+            switch (moviechoice) {
+                case 1: movie_name = "Sardaar Ji 3"; break;
+                case 2: movie_name = "Chal Mera Putt"; break;
+                case 3: movie_name = "Jatt & Juliet"; break;
+                default: movie_name = "Unknown"; cout << "Invalid choice.\n";
             }
-
-            switch (time) {
-                case 1: cout << "15 of front seats are booked and 35 back seats are available at this time\nEach ticket cost 600RS\n"; break;
-                case 2: cout << "25 of front seats are booked and 25 back seats are available at this time\nEach ticket cost 600RS\n"; break;
-                case 3: cout << "30 of front seats are booked and 20 back seats are available at this time\nEach ticket cost 600RS\n"; break;
-                default: cout << "Invalid time selected.\n";
-            }
-
-            cout << "Can you tell how many back seats you want to book? ";
-            cin >> backSeats;
-            ticket_price=600*backSeats;
-            
-            
         }
 
-        if (movieLanguageChoice == 2) {
-            cout << "Available movies for Today:\n";
-            cout << "1. Sardaar ji 3 \n2. Chal mera putt\n3. Jutt && Juliate \nChoose (1-3): ";
-            cin >> movieChoice;
+        cout << "\nSelect show time:\n1. 08:00 AM\n2. 03:00 PM\n3. 07:00 PM\nChoose (1-3): ";
+        int time;
+        cin >> time;
+        switch (time) {
+            case 1: movieTime = "08:00 AM"; 
+			availableSeats = 35; break;
+            case 2: movieTime = "03:00 PM";
+			 availableSeats = 25; break;
+            case 3: movieTime = "07:00 PM";
+			 availableSeats = 20; break;
+            default: movieTime = "Unknown";
+        }
 
-            int time;
-            cout << "\nSelect the movie show time (1-3):\n";
-            cout << "1. 08:00 AM\n2. 03:00 PM\n3. 07:00 PM\n";
-            cin >> time;
-
-            switch (time) {
-                case 1:
-				 movieTime = "08:00 AM";
-				  break;
-                case 2:
-				 movieTime = "03:00 PM";
-				  break;
-                case 3:
-				 movieTime = "07:00 PM";
-				  break;
-                default:
-				 movieTime = "Unknown";
-				  cout << "Invalid movie choice.\n";
-            }
-
-            switch (movieChoice) {
-                case 1:
-				 movie_name = "Sardaar ji 3";
-				  break;
-                case 2:
-				 movie_name = "Chal mera putt";
-				  break;
-                case 3:
-				 movie_name = "Jutt && Juliate";
-				  break;
-                default:
-				 movie_name = "Unknown";
-				  cout << "Invalid movie choice.\n";
-            }
-
-            switch (time) {
-                case 1: cout << "20 of front seats are booked and 30 back seats are available at this time\nEach ticket cost 600RS\n"; break;
-                case 2: cout << "25 of front seats are booked and 25 back seats are available at this time\nEach ticket cost 600RS\n"; break;
-                case 3: cout << "30 of front seats are booked and 20 back seats are available at this time\nEach ticket cost 600RS\n"; break;
-                default: cout << "Invalid time selected.\n";
-            }
-
-            cout << "\nCan you tell how many back seats you want to book? ";
-            cin >> backSeats;
-            ticket_price=600*backSeats;
+        cout << "\nAvailable back seats: " << availableSeats << " & Price: 600 Rs for each\n";
+        cout << "How many seats do you want to book? ";
+        cin >> backSeats;
+        ticket_price = 600 * backSeats;
     }
+
+    void bookingConfirmation() {
+        cout << "\n-------------------------------------------------------------";
+        cout << "\n Booking Successful Your seats have been reserved :-)\n";
+        cout << " Thank you for choosing Plex Cinema\n";
+        cout << " Please arrive 15 minutes before showtime\n";
+        cout << "-------------------------------------------------------------\n";
     }
-	void thanks(){
-	cout << "--------------------------------------------------------------------------------------";
-            cout << "\n\nBooking successfully! Your seat has been reserved\n\nThank you very much for contacting us\n\nCome on " << dayName << " to hall number 4\n\n ";
-}
 };
 
-class customer : public movie {
+class Customer : public Movie {
 public:
-    void ud() {
-        cout << "\n--- Booking Summary ---\n\n";
-        cout << "Customer Name: " << user_name << endl;
-        cout << "Customer Email: " << user_email << endl;
-        cout << "Movie Type: " << movieLanguage << endl;
-        cout << "Movie Day: " << dayName << endl;
-        cout << "Movie Time: " << movieTime << endl;
-        cout << "Movie Selected: " << movie_name << endl;
-        cout << "Seat Reserved: " << backSeats << endl;
-         cout << "Total Bill: " << ticket_price << endl;
+    void bookingSummary() {
+        cout << "\n----------- BOOKING SUMMARY ---------------\n";
+        cout << "Customer Name   : " << user_name << endl;
+        cout << "Customer Email  : " << user_email << endl;
+        cout << "Movie Language  : " << movielanguage << endl;
+        cout << "Movie Name      : " << movie_name << endl;
+        cout << "Day             : " << dayname << endl;
+        cout << "Show Time       : " << movieTime << endl;
+        cout << "Seats Reserved  : " << backSeats << endl;
+        cout << "Total Bill      : " << ticket_price << " Rs\n";
+        cout << "----------------------------------------------\n";
     }
 };
 
 int main() {
-    cout << "--------- Welcome to Plex Cinema ---------\n\n";
-    cout << "             Hope you're doing good               \n\n";
+    cout << "-------- Welcome to Plex Cinema ----------\n";
+    cout << "          Hope you're doing good<3        \n\n";
 
-    customer m;
-    m.user_details();
-    m.movie_days();
-    m.thanks();
+    Customer c;
+    c.getUserDetails();
+    c.choose_movie();
+    c.bookingConfirmation();
+    c.bookingSummary();
 
-    m.ud();
-   
     return 0;
 }
